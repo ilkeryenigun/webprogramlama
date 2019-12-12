e3048d96dc3e4b600a3ed1b89d1b2012f98219b7
İlker Yenigün <ilker.yenigun@ogr.sakarya.edu.tr>
2019-12-12 12:44:55 +0300

    Proje oluşturuldu

---
+++ WebProgramlamaProjesi/WebProgramlamaProjesi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebProgramlamaProjesi.Startup))]
+namespace WebProgramlamaProjesi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
